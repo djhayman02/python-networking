8fb470bd39ea3e6dcb42e3cebd7bd2100bcae54f
Dan Hayman <djhayman02@gmail.com>
2023-08-25 17:51:47 +0100

    fix URL

--- parse.py
+++ parse.py
@@ -9,7 +9,7 @@ import requests
 
 host = "192.168.51.68"
 port = 8080
-url = urlopen("http://www.megacorpone.com:" + str(port) + "/crawling")
+url = urlopen("http://192.168.51.68:" + str(port) + "/crawling")
 
 page = url.read()
 soup = BeautifulSoup(page, features="html.parser")
