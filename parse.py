b98fa8737ec28fd34232ae00038eabb80fdc88c5
Dan Hayman <djhayman02@gmail.com>
2023-08-25 17:42:51 +0100

    list URls from crawling

--- parse.py
+++ parse.py
@@ -2,10 +2,16 @@
 #parse.py
 
 import urllib3
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import socket
+import requests
 
-http = urllib3.PoolManager()
+host = "192.168.51.68"
+port = 8080
+url = urlopen("http://www.megacorpone.com:" + port + "/crawling")
 
-url = 'http://www.megacorpone.com'
+page = url.read()
+soup = BeautifulSoup(page, features="html.parser")
 
-response = http.request('GET', url)
-print(response.data.decode('utf-8'))
+print(soup.get_text())
